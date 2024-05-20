45358190347706c6a51ab1651565e3f7d779a4d4
rafie <amirali.rafie.03@gmail.com>
2024-05-21 03:01:41 +0330

    added technology link

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,6 @@ app_name = "projects"
 
 urlpatterns = [
     path("", views.ProjectsList.as_view(), name="projects"),
+    path("technology/<slug:slug>/", views.TechnologyProjectsList.as_view(), name="technology"),
+    path("category/<slug:slug>/", views.CategoryProjectsList.as_view(), name="category"),
 ]
