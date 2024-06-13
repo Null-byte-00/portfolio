a61400adc7997e8355c80a3344a4270dffe1ee3f
rafie <amirali.rafie.03@gmail.com>
2024-06-14 00:47:11 +0330

    fixed relations in project serializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,24 @@ from mainapp.models import SiteDescription, Resume, Experience
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='name'
+     )
+    
+    technologies = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+     )
+    
     class Meta:
         model = Project
         fields = ['title', 'description', 'link', 'category', 'technologies', 'date']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['title', ]
