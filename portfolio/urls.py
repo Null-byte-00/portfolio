a2e17b15f1d5e0a2ac19636e7784d90c9d404801
rafie <amirali.rafie.03@gmail.com>
2026-05-05 19:55:39 +0330

    added dotenv secret key

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("", include("mainapp.urls")),
     path("projects/", include("projects.urls")),
     path("articles/", include("articles.urls")),
-    path("api/", include("api.urls")),
+    #path("api/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
